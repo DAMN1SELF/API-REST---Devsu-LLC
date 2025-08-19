2ed37cc22e39fc52bfbe913698882ff95e59e8c2
DAMN1SELF <damn1self@gmail.com>
2025-08-19 02:08:05 -0500

    [Feature-Common] Mensajes predeterminados de API REST

---
+++ API-REST DEVSU/INCHE.Common/Constants/ApplicationConstants.cs
@@ -0,0 +1,29 @@
+﻿
+namespace INCHE.Common.Constants
+{
+	public static class Messages
+	{
+
+		public const string RecordCreated = "El registro se creó correctamente";
+		public const string RecordRetrieved = "El registro se obtuvo correctamente";
+		public const string RecordsRetrieved = "Los registros se obtuvieron correctamente";
+		public const string RecordUpdated = "El registro se actualizó correctamente";
+		public const string RecordPatch = "El registro se actualizó parcialmente de forma correcta";
+        public const string RecordDeleted = "El registro se eliminó correctamente";
+
+		public const string RecordNotFound = "No se encontró el registro";
+		public const string NoRecordsFound = "No se encontraron registros";
+
+		public const string RecordAlreadyDeleted = "El registro ya ha sido eliminado";
+		public const string RecordAlreadyExists = "El registro ya existe";
+
+		public const string RecordCreationFailed = "No se pudo crear el registro";
+		public const string RecordUpdateFailed = "No se pudo actualizar el registro";
+		public const string RecordPatchFailed = "No se pudo actualizar parcialmente los datos del registro";
+        public const string RecordDeleteFailed = "No se pudo eliminar el registro";
+	
+        public const string InvalidRecordId = "El identificador no es válido";
+		public const string MissingData = "Faltan datos obligatorios";
+
+	}
+}
