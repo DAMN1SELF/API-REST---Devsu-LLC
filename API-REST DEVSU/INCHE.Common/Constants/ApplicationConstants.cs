90f5baec0810fe9dad191e2c10b5e2097832d13e
DAMN1SELF <damn1self@gmail.com>
2025-08-21 06:05:48 -0500

    [feature Common ] Agregar mas constantes

--- API-REST DEVSU/INCHE.Common/Constants/ApplicationConstants.cs
+++ API-REST DEVSU/INCHE.Common/Constants/ApplicationConstants.cs
@@ -20,10 +20,17 @@ namespace INCHE.Common.Constants
 		public const string RecordCreationFailed = "No se pudo crear el registro";
 		public const string RecordUpdateFailed = "No se pudo actualizar el registro";
 		public const string RecordPatchFailed = "No se pudo actualizar parcialmente los datos del registro";
-        public const string RecordDeleteFailed = "No se pudo eliminar el registro";
+        public const string RecordDeletedFailed = "No se pudo eliminar el registro";
 	
         public const string InvalidRecordId = "El identificador no es válido";
 		public const string MissingData = "Faltan datos obligatorios";
 
-	}
+		public const string DuplicateKey = "Ya existe un registro con la misma clave";
+		public const string PasswordRequired = "La contraseña es obligatoria";
+
+		public const string Unauthorized = "No autorizado";
+        public const string RouteIdDoesNotMatchBodyId = "El identificador de la ruta no coincide con el del cuerpo de la solicitud";
+		public const string InvalidCredentials = "Credenciales inválidas";
+		public const string ClientHasLinkedAccounts = "El cliente tiene cuentas vinculadas y no puede ser eliminado";
+    }
 }
